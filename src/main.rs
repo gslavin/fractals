a2131e4615725f28d11d97976cec1b990f78f10b
George Slavin <george.r.slavin@gmail.com>
2016-06-05 00:29:13 -0400

    Added burning ship fractal

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,35 @@ extern crate num;
 use num::complex::*;
 use bmp::{Image, Pixel};
 
-const SIZE: u32 = 512;
+const SIZE: u32 = 256;
 const ESCAPE_LIMIT: f64 = 1000000.0;
 const ITER_LIMIT: u32 = 200;
-const WIDTH: f64 = 3.0;
-const HEIGHT: f64 = 3.0;
-const C_VAL: Complex64 = Complex{re: 0.25, im: 0.5};
+const WIDTH: f64 = 0.005;
+const HEIGHT: f64 = 0.010;
+const X_CENTER: f64 = 1.941;
+const Y_CENTER: f64 = 0.004;
 
-// Mendelbrot transform
-fn fractal_func(c: Complex64) -> Box<Fn(Complex64) -> Complex64> 
+// Burning ship
+#[inline]
+fn burning_ship() -> Box<Fn(Complex64, Complex64) -> Complex64> 
 {
-    return Box::new(move |z: Complex64| z.powf(2.0) + c);
+    return Box::new(move |z: Complex64, c: Complex64| (Complex{re: z.re.abs(), im: z.im.abs()}).powf(2.0) + c);
 }
 
-fn run_until_escape(z: Complex64, escape_limit: f64, iter_limit: u32,
-                    gen_fn: Box<Fn(Complex64) -> Complex64>) -> u32
+// Mendelbrot 
+fn mandelbrot() -> Box<Fn(Complex64, Complex64) -> Complex64> 
 {
-    let mut temp = z;
+    return Box::new(move |z: Complex64, c: Complex64| z.powf(2.0) + c);
+}
+
+fn run_until_escape(gen_fn: Box<Fn(Complex64, Complex64) -> Complex64>,
+    c: Complex64, escape_limit: f64, iter_limit: u32) -> u32
+{
+    let mut z: Complex64 = Complex64{ re: 0.0, im: 0.0};
     let mut n: u32 = 0; 
-    while temp.norm_sqr() < escape_limit && n < iter_limit {
+    while z.norm_sqr() < escape_limit && n < iter_limit {
         n += 1;
-        temp = (*gen_fn)(temp);
+        z = (*gen_fn)(z, c);
     }
 
     return n;
@@ -36,11 +44,10 @@ fn main() {
 
     let coordinates: Vec<(u32, u32)> = (0..SIZE).flat_map(move |x| (0..SIZE).map(move |y| (x,y))).collect();
     for &(x, y) in &coordinates {
-        let float_x: f64 = WIDTH*((x as f64)/(SIZE as f64) - 0.5);
-        let float_y: f64 = HEIGHT*((y as f64)/(SIZE as f64) - 0.5);
-        let iterations = run_until_escape(Complex{re:float_x, im:float_y}, 
-                                          ESCAPE_LIMIT, ITER_LIMIT,
-                                          fractal_func(C_VAL));
+        let float_x: f64 = WIDTH*((x as f64)/(SIZE as f64) - 0.5) - X_CENTER;
+        let float_y: f64 = HEIGHT*((y as f64)/(SIZE as f64) - 0.5) - Y_CENTER;
+        let iterations = run_until_escape(burning_ship(), Complex{re: float_x, im: float_y}, 
+                                          ESCAPE_LIMIT, ITER_LIMIT);
         pixels[(SIZE*x + y) as usize] = iterations;
     }
 
